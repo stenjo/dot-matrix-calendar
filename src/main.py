5f7cad82d3be85600a698ee8606d6c750b5659a0
Sten Johnsen <sten.johnsen@bouvet.no>
2024-02-07 22:50:51 +0100

    wip

--- src/main.py
+++ src/main.py
@@ -1,39 +1,40 @@
 from machine import Pin, SPI, PWM
 from time import sleep
 from scroller import Scroller
-from modules.dateHandling import dayText
+
+from dateHandling import dayText
 import network
 
-from uoauth2 import DeviceAuth
+# from uoauth2 import DeviceAuth
 
-# For more information on how to create clients 
-# Look at: https://developers.google.com/identity/protocols/oauth2/limited-input-device
+# # For more information on how to create clients
+# # Look at: https://developers.google.com/identity/protocols/oauth2/limited-input-device
 
-device_auth = DeviceAuth(
-    client_id='648445354032-mv5p4b09hcj0116v57pnkmp42fn8m220.apps.googleusercontent.com',
-    client_secret='9aeN3LGr0yq4TYjwGcfUVJKo',
-    discovery_endpoint='https://accounts.google.com/.well-known/openid-configuration',
-    scopes=list(['openid'])
-)
+# device_auth = DeviceAuth(
+#     client_id="648445354032-mv5p4b09hcj0116v57pnkmp42fn8m220.apps.googleusercontent.com",
+#     client_secret="9aeN3LGr0yq4TYjwGcfUVJKo",
+#     discovery_endpoint="https://accounts.google.com/.well-known/openid-configuration",
+#     scopes=list(["openid"]),
+# )
 
-# Discover OpenID endpoints
-device_auth.discover()
+# # Discover OpenID endpoints
+# device_auth.discover()
 
-# Start authorization process
-device_auth.authorize()
+# # Start authorization process
+# device_auth.authorize()
 
-# Use the user-code and verification URL to show some UI to the user
-# To complete the authorization process.
-user_code = device_auth.user_code
-verification_url = device_auth.verification_url
+# # Use the user-code and verification URL to show some UI to the user
+# # To complete the authorization process.
+# user_code = device_auth.user_code
+# verification_url = device_auth.verification_url
 
-print(user_code, verification_url)
+# print(user_code, verification_url)
 
-# Check for completed authorization
-device_auth.check_authorization_complete()
+# # Check for completed authorization
+# device_auth.check_authorization_complete()
 
-# Fetch a valid access token
-print(device_auth.token())
+# # Fetch a valid access token
+# print(device_auth.token())
 
 spi = SPI(1, baudrate=115000)
 ss = Pin(15, Pin.OUT)
@@ -41,7 +42,7 @@ display = Scroller(spi, ss, 4)
 
 event = {"start": {"date": "2023-10-23"}, "summary": "Event summary"}
 
-
+print(dayText(event))
 # display.scroll_text(dayText(event))
 
 sta_if = network.WLAN(network.STA_IF)
