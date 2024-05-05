511512d222692ccf7defec50b764307d02bd8873
Sten Johnsen <sten@thinea.no>
2024-05-05 06:33:58 +0200

    Fixed local time

--- modules/time.py
+++ modules/time.py
@@ -77,3 +77,52 @@ def strftime(datefmt, ts):
     val = ftime.getvalue()
     ftime.close()
     return val
+
+################################################################
+# Function time.localtime()
+#
+# Returns the local time for Norway with DST
+#
+# Credit to @steka for the code https://github.com/orgs/micropython/discussions/11173#discussioncomment-7524615
+#
+
+def localtime(secs: int | None = None) -> tuple[int, int, int, int, int, int, int, int]:
+    """Returns Norwegian local time.
+
+    According to [Wikipedia](https://en.wikipedia.org/wiki/Time_in_Norway)
+    does Norway observe daylight saving time
+    from: the last Sunday in March (02:00 CET)
+    to:   the last Sunday in October (03:00 CEST)
+    """
+
+    def last_sunday(year: int, month: int, hour: int, minute: int) -> int:
+        """Get the time of the last sunday of the month
+        It returns an integer which is the number of seconds since Jan 1, 2000, just like mktime().
+        """
+
+        # Get the UTC time of the last day of the month
+        seconds = mktime((year, month + 1, 0, hour, minute, 0, None, None))
+
+        # Calculate the offset to the last sunday of the month
+        (year, month, mday, hour, minute, second, weekday, yearday) = gmtime(seconds)
+        offset = (weekday + 1) % 7
+
+        # Return the time of the last sunday of the month
+        return mktime((year, month, mday - offset, hour, minute, second, None, None))
+
+    utc = gmtime(secs)
+
+    # Find start date for daylight saving, i.e. last Sunday in March (01:00 UTC)
+    start_secs = last_sunday(year=utc[0], month=3, hour=1, minute=0)
+
+    # Find stop date for daylight saving, i.e. last Sunday in October (01:00 UTC)
+    stop_secs = last_sunday(year=utc[0], month=10, hour=1, minute=0)
+
+    utc_secs = mktime(utc)
+    if utc_secs >= start_secs and utc_secs < stop_secs:
+        delta_secs = 2 * 60 * 60  # Norwegian summer time (CEST or UTC + 2h)
+    else:
+        delta_secs = 1 * 60 * 60  # Norwegian normal time (CET or UTC + 1h)
+
+    return gmtime(utc_secs + delta_secs)
+
