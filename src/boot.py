68ebe5966f2bf8d9f6266056c4930d5e048fcce4
Sten Johnsen <sten@thinea.no>
2024-05-20 07:23:30 +0200

    working

--- src/boot.py
+++ src/boot.py
@@ -2,17 +2,11 @@
 # import frozen_wifi_setup.py
 import frozen_wifi_setup
 import frozen_micro_web_srv_2
-
-# from time import sleep
 import os, machine
-#os.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 gc.collect()
 
-# boot.py -- run on boot-up
 import network
-# import mpy_env
-
 import ntptime
 import time
 
@@ -21,29 +15,6 @@ from wifi_setup.wifi_setup import WiFiSetup
 ws = WiFiSetup("dot-matrix-calendar")
 sta = ws.connect_or_setup()
 del ws
-
-# import webrepl
-# webrepl.start()
-
-# from mpy_env import load_env, get_env, put_env
-
-# mpy_env.load_env()
-# def do_connect():
-#     sta_if = network.WLAN(network.STA_IF)
-#     if not sta_if.isconnected():
-#         print("")
-#         print("--------------------------------")
-#         print("connecting to network...")
-#         sta_if.active(True)
-#         sta_if.connect(mpy_env.get_env("WIFI_SSID"), mpy_env.get_env("WIFI_PASSWD"))
-#         countDown = 20
-#         while not sta_if.isconnected() and countDown:
-#             sleep(1)
-#             countDown = countDown - 1
-#             print("... trying again ...")
-#     print("network config:", sta_if.ifconfig())
-
-# do_connect()
 
 #if needed, overwrite default time server
 ntptime.host = "1.europe.pool.ntp.org"
