8fa592ead3707e0ff5a83f528792cbc0b6264e0d
Sten Otto Johnsen <46670655+steoj@users.noreply.github.com>
2024-07-08 09:11:32 +0200

    fix(main.py): updated Calendar.py to handle days ahead param (#37)
    * feat(ical): handle large ics files
    
    * better structure
    
    * passing
    
    * compiles
    
    * adding mrequests
    
    * chunks
    
    * chunks2
    
    * cleanup
    
    * Running test in python
    
    * passing calendar tests
    
    * works
    
    * ignore credentials
    
    * update
    
    * 8 modules
    
    * fixed days ahead and chunked refresh
    
    * stable
    
    * 8-module matrix
    
    ---------
    
    Co-authored-by: Sten Johnsen <sten@thinea.no>

--- src/main.py
+++ src/main.py
@@ -8,11 +8,9 @@ import time
 import network
 from wifi_setup.wifi_setup import WiFiSetup
 import gc
-gc.collect()
 
 # (8x8 blocks, spi host, clock speed, CS pin)
 m = Matrix8x8(8,20,1)
-
 m.clear()
 m.init()
 
@@ -26,64 +24,48 @@ m.marquee(sta_if.ifconfig()[0] + " " + version)
 while not m.scroll():
     pass
 
-print(gc.mem_free())
-
-(year, month, day, hour, min, sec, _, _) = time.localtime()
-print("{}:{:02}:{:02}".format(hour, min, sec))
-m.write("{}/{}-{}".format(day, month, year), True)
-time.sleep(3)  # import time(1)
-
-
-days_ahead = 7
-c=Calendar()
-c.start(time.gmtime())
-c.end((datetime.now() + timedelta(days_ahead)).timetuple())
-
+c=Calendar(daysAhead=14)
 c.parseURL('webcal://files-f2.motorsportcalendars.com/no/f2-calendar_p_q_sprint_feature.ics')
 c.parseURL('webcal://files-f3.motorsportcalendars.com/no/f3-calendar_p_q_sprint_feature.ics')
-done = False
+c.parseURL('https://calendar.google.com/calendar/ical/ht3jlfaac5lfd6263ulfh4tql8%40group.calendar.google.com/public/basic.ics')
 
 def displayClock(m):
     (_, _, _, hour, min, sec, _, _) = time.localtime()
-    while sec % 15 != 0:
-        if sec % 2 == 0:
-            m.write("{:02}:{:02}".format(hour, min), True)
-        else:
-            m.write("{:02} {:02}".format(hour, min), True)
-        (_, _, _, hour, min, sec, _, _) = time.localtime()
+    if sec % 15 == 0: return True
+    if sec % 2 == 0:
+        m.write("{:02}:{:02}".format(hour, min), True)
+    else:
+        m.write("{:02} {:02}".format(hour, min), True)
+    return False
 
+def reconnectIfNotConnected(sta_if):
+    sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+    if not sta_if.isconnected():
+        while not sta_if.isconnected():
+            sta_if.active(True)
+            print("Trying to connect...")
+            time.delay(1)
+        ws = WiFiSetup("dot-matrix-calendar")
+        sta = ws.connect_or_setup()
+        del ws
+
+scrollDone = False
+clockDone = False
 try:
     event = c.first()
-    if event: m.marquee(dayText(event))
-    
-finally:
-    displayClock(m)
-    
-try:
     while True:
-        done = m.scroll(False)
-        if done:
-            displayClock(m)
-            event = c.next()
+        if scrollDone is False: scrollDone = m.scroll(False)
+        if scrollDone is True: clockDone = displayClock(m)
+        if scrollDone and clockDone:
             if not event:
-                gc.collect()
-                items = c.refresh(time.gmtime(), (datetime.now() + timedelta(days_ahead)).timetuple())
-                print(gc.mem_free())
+                items = c.refresh()
                 event = c.first()
-                # print("Refetched {:02} calendar items.".format(items))
-                sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
-                if not sta_if.isconnected():
-                    while not sta_if.isconnected():
-                        sta_if.active(True)
-                        print("Trying to connect...")
-                        time.delay(1)
-
-                    ws = WiFiSetup("dot-matrix-calendar")
-                    sta = ws.connect_or_setup()
-                    del ws
+                reconnectIfNotConnected(sta_if)
                     
             if event:
                 m.marquee(dayText(event))
+            event = c.next()
+            scrollDone = False
 except KeyboardInterrupt:
     print("\nControl-C pressed. Cleaning up and exiting.")
 finally:
