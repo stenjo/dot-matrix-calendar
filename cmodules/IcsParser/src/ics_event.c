c0ee082286b68553ac79c2d31f85668ec88732d5
Sten Johnsen <sten@thinea.no>
2024-07-22 17:30:11 +0200

    passing

--- cmodules/IcsParser/src/ics_event.c
+++ cmodules/IcsParser/src/ics_event.c
@@ -135,7 +135,7 @@ char *extract_property(const char *data, const char *property, const char *end) 
         strncpy(value, start, len);
         value[len] = '\0';
     }
-    remove_all_chars(value, '\\');
+    nukechar(value, '\\');
     return value;
 }
 
--- cmodules/IcsParser/test/test_ics_parser.c
+++ cmodules/IcsParser/test/test_ics_parser.c
@@ -652,5 +652,31 @@ void test_parse_with_chunks2(void) {
     freeIcs(&ics);
 }
 
+void test_parse_should_handle_commas_in_summary(void) {
+    ics_t ics;
+    initIcs(&ics);
+
+    const char *data =  "BEGIN:VEVENT\r\n"
+                        "DTSTART;VALUE=DATE:20190422\r\n"
+                        "DTEND;VALUE=DATE:20190423\r\n"
+                        "DTSTAMP:20240424T062905Z\r\n"
+                        "UID:6qchqqlee209carvhfo3ino68g@google.com\r\n"
+                        "CREATED:20190420T092806Z\r\n"
+                        "LAST-MODIFIED:20190420T093123Z\r\n"
+                        "SEQUENCE:2\r\n"
+                        "STATUS:CONFIRMED\r\n"
+                        "SUMMARY:Laks\\, stappe og brokkoli med hollandaise saus\r\n"
+                        "TRANSP:TRANSPARENT\r\n"
+                        "END:VEVENT\r\n";
+
+    // Initialize buffer with data and then call getEvent
+
+    size_t count = parseIcs(&ics, data);
+
+    TEST_ASSERT_EQUAL(1, count);
+    TEST_ASSERT_EQUAL_STRING("Laks, stappe og brokkoli med hollandaise saus", ics.events[0]->summary);
+
+    freeIcs(&ics);
+}
 
 #endif // TEST
